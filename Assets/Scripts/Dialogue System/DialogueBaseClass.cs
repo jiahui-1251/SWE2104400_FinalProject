98a6964b882ab33a38605d1d47834c1b23e2bb86
jiahui-1251 <jiahui.tan02@gmail.com>
2023-07-03 11:14:51 +0800

    added script

---
+++ Assets/Scripts/Dialogue System/DialogueBaseClass.cs
@@ -0,0 +1,19 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngne.UI;
+
+// nameSpace DialogueSystem
+// {
+//     public class DialogueBaseClass : MonoBehaviour
+//     {
+//         private IEnumerator WriteText(string input, Text textHolder)
+//         {
+//             for(int i = 0; i < input.length; i++)
+//             {
+//                 textHolder.text += input[i];
+//                 yield return new WaitForSeconds(0.1f);
+//             }
+//         }
+//     }
+// }
